bfb1be2a2c1ddc0f245fdb612859bc0e70c561ef
alphaseneca <alphatheseneca@protonmail.com>
2023-01-10 14:43:17 +0545

    Initial commit

---
+++ For Students Graphics Lab/OpenGL Lab/Readme.txt
@@ -0,0 +1,10 @@
+
+https://www.transmissionzero.co.uk/software/freeglut-devel/
+
+	Go to :freeglut 3.0.0 MSVC Package
+           and
+		Download freeglut 3.0.0 for MSVC 
+
+
+
+https://www3.ntu.edu.sg/home/ehchua/programming/opengl/CG_Introduction.html
---
+++ Tasks/task3/task3.cpp
@@ -0,0 +1,70 @@
+#include<graphics.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<dos.h>
+#include<iostream>
+using namespace std;
+void draw(int xm,int ym,int x, int y){
+
+    	
+        putpixel(xm+x,ym-y,1);
+        
+        putpixel(xm+y,ym-x,2);
+       
+        putpixel(xm+y,ym+x,3);
+       
+        putpixel(xm+x,ym+y,4);
+     
+        putpixel(xm-x,ym+y,5);
+       
+        putpixel(xm-y,ym+x,6);
+       
+        putpixel(xm-y,ym-x,7);
+        
+        putpixel(xm-x,ym-y,8);
+       
+}
+void calculate(int r,int xm,int ym){
+	int x=0;
+	
+    int y=r;
+    float p=1-r;
+    
+    	while(x<=y)
+    	
+    {
+    	delay(10);
+    	draw(xm,ym,x,y);
+        if(p<0)
+        {
+        	x++;
+            p+=(2*x)+1;
+        }
+        else
+        {
+            p=p+(2*(x-y))+1;
+            y--;
+            x++;
+        }
+        
+    }
+}
+int main()
+{
+	float xm,ym;
+	initwindow(400,400);
+    cout<<"Enter radius:";
+	int r;
+    cin>>r;
+	cout<<"Enter the center of the circle: ";
+	cin>>xm>>ym;
+	
+	for(int i=0;i<=r;i++){
+		calculate(i,xm+i,ym+i);
+	}   
+    
+    getch();
+    closegraph();
+    return 0;
+}
+
